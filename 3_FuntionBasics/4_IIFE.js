e7348c3f8979e891da1b7098f280e4024359176b
Mr-Ayush25 <141553855+Mr-Ayush25@users.noreply.github.com>
2023-08-12 10:06:29 +0000

    IIFE IMP

---
+++ 3_FuntionBasics/4_IIFE.js
@@ -0,0 +1,31 @@
+// IIFe means Immediately invoked Function Expression.
+// these function is used to stop poppulating the pollution of Global Scope.
+// () writing function inside a parenthesis will be block scoped.
+
+// ex Normal IIFe
+/*
+(function trial(){
+    console.log("Hello There")
+})()
+
+(function trial2(){
+    console.log("Hello There")
+})()
+*/
+// By doing above thing it will throw an error beacuse the first IIFE has started but it doesn't know that where to stop. we can use ";" to define the stage.
+// Now this will work
+(function trial(){
+    console.log("Hello There")
+})();
+
+(function trial2(){
+    console.log("Hello There")
+})();
+// IIFE can be named or anynommous function.
+(()=>{
+    console.log("Hello Ayush")
+})();
+// We can pass arguments as well.
+((name)=>{
+    console.log(`Hello ${name}`)
+})("Ayush");
